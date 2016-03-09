a24bc4c4288fe4748010b9afe46a829cd27388a0
Cristina Gallego <cristina@ubuntu>
2016-03-09 11:43:13 +0100

    Clase

--- webapp.py
+++ webapp.py
@@ -3,7 +3,6 @@
 """
 webApp class
  Root for hierarchy of classes implementing web applications
-
  Copyright Jesus M. Gonzalez-Barahona and Gregorio Robles (2009-2015)
  jgb @ gsyc.es
  TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
@@ -15,7 +14,6 @@ import socket
 
 class webApp:
     """Root of a hierarchy of classes implementing web applications
-
     This class does almost nothing. Usually, new classes will
     inherit from it, and by redefining "parse" and "process" methods
     will implement the logic of a web application in particular.
@@ -28,7 +26,6 @@ class webApp:
 
     def process(self, parsedRequest):
         """Process the relevant elements of the request.
-
         Returns the HTTP code for the reply, and an HTML page.
         """
 
